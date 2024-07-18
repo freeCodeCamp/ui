e6588ea63938365043c64f35bbc8f03a237316ee
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-07-18 13:01:39 +0700

    fix: dropdown icon alignment

--- src/drop-down/drop-down.tsx
+++ src/drop-down/drop-down.tsx
@@ -33,7 +33,7 @@ const dropDownItems =
 	"list-none bg-foreground-secondary text-center border-1 border-solid border-background-quaternary focus:outline-transparent origin-top-right absolute w-full min-w-max py-1 px-0 z-10";
 const dropUpItems = dropDownItems + " transform -translate-y-full top-0";
 const toggleClassNames =
-	"cursor-pointer border-3 border-solid w-full block text-center touch-manipulation bg-background-quaternary text-foreground-secondary px-3 py-1.5 mt-[0.5px] relative hover:bg-foreground-secondary hover:text-background-secondary border-foreground-secondary";
+	"cursor-pointer border-3 border-solid w-full flex items-center justify-center text-center touch-manipulation bg-background-quaternary text-foreground-secondary px-3 py-1.5 mt-[0.5px] relative hover:bg-foreground-secondary hover:text-background-secondary border-foreground-secondary ";
 
 export const MenuItems = React.forwardRef<HTMLDivElement, MenuItemsProps>(
 	({ children, className }, ref): JSX.Element => {
