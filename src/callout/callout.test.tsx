f47ad7345605ccb2866ff179c10fbe9407b48178
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-07-19 22:45:20 +0700

    test: add type tests

--- src/callout/callout.test.tsx
+++ src/callout/callout.test.tsx
@@ -10,3 +10,27 @@ describe("<Callout />", () => {
 		expect(screen.getByText("Hello World")).toBeInTheDocument();
 	});
 });
+
+// ------------------------------
+// Type tests
+// ------------------------------
+
+// @ts-expect-error - Callout does not accept `role`
+<Callout variant="success" role="alert">
+	Hello World
+</Callout>;
+
+// @ts-expect-error - Callout does not accept `role`
+<Callout variant="info" role="alert">
+	Hello World
+</Callout>;
+
+// @ts-expect-error - Callout does not accept `role`
+<Callout variant="warning" role="alert">
+	Hello World
+</Callout>;
+
+// @ts-expect-error - Callout does not accept `role`
+<Callout variant="danger" role="alert">
+	Hello World
+</Callout>;
