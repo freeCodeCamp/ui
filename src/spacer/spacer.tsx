2923d59572cb3662d56ef642ae6f1d49b87b3e9e
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-07-24 10:44:22 -0700

    Apply suggestions from code review
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- src/spacer/spacer.tsx
+++ src/spacer/spacer.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-type Size = "xxs" | "xs" | "s" | "m" | "l" | "xl" | "xxl";
+const sizes = {
+	xxs: "h-[5px]",
+	xs: "h-[10px]",
+	s: "h-[20px]",
+	m: "h-[30px]",
+	l: "h-[60px]",
+	xl: "h-[90px]",
+	xxl: "h-[180px]",
+} as const;
 
 interface SpacerProps {
 	/**
@@ -13,29 +21,11 @@ interface SpacerProps {
 	 * - xl: 90px
 	 * - xxl: 180px
 	 */
-	size: Size;
+	size: keyof typeof sizes;
 }
 
 export const Spacer = ({ size }: SpacerProps) => {
-	let cls;
-
 	// NOTE: Do not construct class names dynamically
 	// https://tailwindcss.com/docs/content-configuration#classes-arent-generated
-	if (size === "xxs") {
-		cls = "h-[5px]";
-	} else if (size === "xs") {
-		cls = "h-[10px]";
-	} else if (size === "s") {
-		cls = "h-[20px]";
-	} else if (size === "m") {
-		cls = "h-[30px]";
-	} else if (size === "l") {
-		cls = "h-[60px]";
-	} else if (size === "xl") {
-		cls = "h-[90px]";
-	} else if (size === "xxl") {
-		cls = "h-[180px]";
-	}
-
-	return <div className={cls} />;
+	return <div className={sizes[size]} />;
 };
