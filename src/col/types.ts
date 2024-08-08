df30d2bfb178a69b867b0c327be0eb48e851f0f4
Gagan <g.deepsingh1@gmail.com>
2024-08-08 11:04:59 -0600

    add option in xs prop

--- src/col/types.ts
+++ src/col/types.ts
@@ -1,7 +1,7 @@
 export interface ColProps extends React.HTMLAttributes<HTMLDivElement> {
 	className?: string;
 	children?: React.ReactNode;
-	xs?: 6 | 8 | 10 | 12;
+	xs?: 4 | 6 | 8 | 10 | 12;
 	sm?: 2 | 4 | 5 | 6 | 8 | 10 | 12;
 	md?: 4 | 6 | 8 | 10 | 12;
 	lg?: 6 | 8 | 10;
