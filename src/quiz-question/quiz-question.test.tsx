798ce5a327b266eddfe213e37a024f02e2b52a86
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-09-03 01:26:45 +0700

    add unit tests

---
+++ src/quiz-question/quiz-question.test.tsx
@@ -0,0 +1,163 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import { QuizQuestion } from "./quiz-question";
+import userEvent from "@testing-library/user-event";
+
+describe("<QuizQuestion />", () => {
+	it("should render as a radio group", () => {
+		render(
+			<QuizQuestion
+				question="Lorem ipsum"
+				options={[
+					{ label: "Option 1", value: 1 },
+					{ label: "Option 2", value: 2 },
+					{ label: "Option 3", value: 3 },
+				]}
+				onChange={() => {}}
+			/>,
+		);
+
+		expect(
+			screen.getByRole("radiogroup", { name: "Lorem ipsum" }),
+		).toBeInTheDocument();
+
+		expect(screen.getByRole("radio", { name: "Option 1" })).toBeInTheDocument();
+		expect(screen.getByRole("radio", { name: "Option 2" })).toBeInTheDocument();
+		expect(screen.getByRole("radio", { name: "Option 3" })).toBeInTheDocument();
+	});
+
+	it("should have the `required` attribute set to `true` if the `required` prop is specified", () => {
+		render(
+			<QuizQuestion
+				question="Lorem ipsum"
+				options={[
+					{ label: "Option 1", value: 1 },
+					{ label: "Option 2", value: 2 },
+					{ label: "Option 3", value: 3 },
+				]}
+				onChange={() => {}}
+				required
+			/>,
+		);
+
+		expect(
+			screen.getByRole("radiogroup", { name: "Lorem ipsum" }),
+		).toBeRequired();
+	});
+
+	it("should have the `aria-disabled` attribute set to `true` if the `disabled` prop is specified", () => {
+		render(
+			<QuizQuestion
+				question="Lorem ipsum"
+				options={[
+					{ label: "Option 1", value: 1 },
+					{ label: "Option 2", value: 2 },
+					{ label: "Option 3", value: 3 },
+				]}
+				onChange={() => {}}
+				disabled
+			/>,
+		);
+
+		expect(screen.getByRole("radio", { name: "Option 1" })).toHaveAttribute(
+			"aria-disabled",
+			"true",
+		);
+		expect(screen.getByRole("radio", { name: "Option 2" })).toHaveAttribute(
+			"aria-disabled",
+			"true",
+		);
+		expect(screen.getByRole("radio", { name: "Option 3" })).toHaveAttribute(
+			"aria-disabled",
+			"true",
+		);
+	});
+
+	it("should have the correct `checked` attribute according to the `selectedOption` prop", () => {
+		render(
+			<QuizQuestion
+				question="Lorem ipsum"
+				options={[
+					{ label: "Option 1", value: 1 },
+					{ label: "Option 2", value: 2 },
+					{ label: "Option 3", value: 3 },
+				]}
+				onChange={() => {}}
+				selectedOption={2}
+			/>,
+		);
+
+		expect(screen.getByRole("radio", { name: "Option 1" })).not.toBeChecked();
+		expect(screen.getByRole("radio", { name: "Option 2" })).toBeChecked();
+		expect(screen.getByRole("radio", { name: "Option 3" })).not.toBeChecked();
+	});
+
+	it("should call `onChange` with the value of the selected option", async () => {
+		const onChange = jest.fn();
+
+		render(
+			<QuizQuestion
+				question="Lorem ipsum"
+				options={[
+					{ label: "Option 1", value: 1 },
+					{ label: "Option 2", value: 2 },
+					{ label: "Option 3", value: 3 },
+				]}
+				onChange={onChange}
+			/>,
+		);
+
+		await userEvent.click(screen.getByRole("radio", { name: "Option 2" }));
+
+		expect(onChange).toHaveBeenCalledTimes(1);
+		expect(onChange).toHaveBeenCalledWith({
+			questionId: "Lorem ipsum",
+			selectedOption: 2,
+		});
+	});
+
+	it("should render the correct state properly", () => {
+		render(
+			<QuizQuestion
+				question="Lorem ipsum"
+				options={[
+					{ label: "Option 1", value: 1 },
+					{ label: "Option 2", value: 2 },
+					{ label: "Option 3", value: 3 },
+				]}
+				onChange={() => {}}
+				validation={{
+					state: "correct",
+					message: "Correct.",
+				}}
+			/>,
+		);
+
+		expect(
+			screen.getByRole("radiogroup", { name: "Correct. Lorem ipsum" }),
+		).toBeInTheDocument();
+	});
+
+	it("should render the incorrect state properly", () => {
+		render(
+			<QuizQuestion
+				question="Lorem ipsum"
+				options={[
+					{ label: "Option 1", value: 1 },
+					{ label: "Option 2", value: 2 },
+					{ label: "Option 3", value: 3 },
+				]}
+				onChange={() => {}}
+				validation={{
+					state: "incorrect",
+					message: "Incorrect.",
+				}}
+			/>,
+		);
+
+		expect(
+			screen.getByRole("radiogroup", { name: "Incorrect. Lorem ipsum" }),
+		).toBeInTheDocument();
+	});
+});
