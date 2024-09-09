c2825595e210058377aa0b72a225856216dd13e9
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-09-10 04:29:32 +0700

    fix: remove getCodeLang export

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,4 @@ export { HelpBlock } from "./help-block";
 export { Row } from "./row";
 export { Modal, type ModalProps, type HeaderProps } from "./modal";
 export { Spacer } from "./spacer";
-export { PrismFormatted, getCodeLanguageFromString } from "./prism-formatted";
+export { PrismFormatted } from "./prism-formatted";
