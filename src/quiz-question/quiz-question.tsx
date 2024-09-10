d0329503658663ff2b8164dabdf3a430dbb61614
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-09-11 01:26:15 +0700

    refactor: convert render functions to components

--- src/quiz-question/quiz-question.tsx
+++ src/quiz-question/quiz-question.tsx
@@ -3,9 +3,55 @@ import { RadioGroup } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-import { type QuizQuestionAnswer, type QuizQuestionProps } from "./types";
+import {
+	QuizQuestionValidation,
+	type QuizQuestionAnswer,
+	type QuizQuestionProps,
+} from "./types";
 
 import { Answer } from "./answer";
+
+const QuestionText = ({
+	question,
+	position,
+}: {
+	question: string;
+	position?: number;
+}) => {
+	if (position == null) {
+		return <span className="text-foreground-primary">{question}</span>;
+	}
+
+	return (
+		<span className="text-foreground-primary">
+			{`${position}. ${question}`}
+		</span>
+	);
+};
+
+const ValidationIcon = ({
+	validation,
+}: {
+	validation: QuizQuestionValidation;
+}) => {
+	const { state, message } = validation;
+
+	return state === "correct" ? (
+		<FontAwesomeIcon
+			icon={faCheck}
+			className="text-background-success me-[8px]"
+			aria-label={message}
+			aria-hidden={false}
+		/>
+	) : (
+		<FontAwesomeIcon
+			icon={faXmark}
+			className="text-background-danger me-[8px]"
+			aria-label={message}
+			aria-hidden={false}
+		/>
+	);
+};
 
 /**
  * QuizQuestion is a radio group that allows users to select a single option from a list of multiple options.
@@ -33,42 +79,6 @@ export const QuizQuestion = ({
 		onChange(selectedOption);
 	};
 
-	const maybeRenderValidationIcon = () => {
-		if (!validation) {
-			return;
-		}
-
-		const { state, message } = validation;
-
-		return state === "correct" ? (
-			<FontAwesomeIcon
-				icon={faCheck}
-				className="text-background-success me-[8px]"
-				aria-label={message}
-				aria-hidden={false}
-			/>
-		) : (
-			<FontAwesomeIcon
-				icon={faXmark}
-				className="text-background-danger me-[8px]"
-				aria-label={message}
-				aria-hidden={false}
-			/>
-		);
-	};
-
-	const renderQuestionText = () => {
-		if (position == null) {
-			return <span className="text-foreground-primary">{question}</span>;
-		}
-
-		return (
-			<span className="text-foreground-primary">
-				{`${position}. ${question}`}
-			</span>
-		);
-	};
-
 	return (
 		<RadioGroup
 			onChange={handleChange}
@@ -80,8 +90,8 @@ export const QuizQuestion = ({
 			value={selectedAnswer ?? null}
 		>
 			<RadioGroup.Label className="block mb-[20px]">
-				{maybeRenderValidationIcon()}
-				{renderQuestionText()}
+				{validation && <ValidationIcon validation={validation} />}
+				<QuestionText question={question} position={position} />
 			</RadioGroup.Label>
 
 			{answers.map(({ value, label }) => (
